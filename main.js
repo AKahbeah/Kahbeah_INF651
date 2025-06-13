52d54b433867946e45c2801d51f0bdf66c9793c2
AKahbeah <albenson@mail.fhsu.edu>
2025-06-12 23:21:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const name = "Lauren";
+const age = 22;
+const isStudent = true;
+console.log("Name", name, typeof name); 
+console.log("Age", age, typeof age);    
+console.log("Is Student", isStudent, typeof isStudent); 
+let x, y;
+x = 2;
+y = 5;
+let z = x + y;
+console.log(z)
+let z = x - y;
+console.log(z)
+let z = x * y;
+console.log(z)
+let z = x / y;
+console.log(z)
+const string = "You're never yoo old to play outside.";
+console.log(string)
+console.log("Length:", string.length);
+console.log("First character:", string.charAt(0));
+console.log("Last character:", mySentence.charAt(string.length - 1));
+let number = -4;
+let squareRoot = Math.sqrt(number);
+console.log(squareRoot);
+let num1 = 12;
+let num2 = 8;
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+}
+if (num1 < num2) {
+  console.log("num1 is less than num2");
+}
+if (num1 === num2) {
+  console.log("num1 is equal to num2");
+let isSummer = true;
+let isWinter = false;
+let isSummerAndWinter = isSummer && isWinter; 
+let isSummerOrWinter = isSummer || isWinter; 
+let notIsSummer = !isSummer; 
+console.log("Is it both summer and winter?", isSummerAndWinter);
+console.log("Is it either summer or winter?", isSummerOrWinter);
+console.log("Is it not summer?", notIsSummer);
+const firstName = "Marcel";
+const lastName = "Pierce";
+const greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
